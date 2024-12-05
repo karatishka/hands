87b3fb60a7a807972bd317d5dd098f5271d9f47b
Александр Жигунов <karatishka2002@gmail.com>
2024-12-05 14:23:32 +0300

    Article

---
+++ resources/js/Pages/Articles/Index.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+    <h1>Index</h1>
+</template>
+
+<style scoped>
+
+</style>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -16,14 +15,16 @@ use Inertia\Inertia;
 |
 */
 
-Route::get('/', function () {
+Route::get('/', [\App\Http\Controllers\ArticleController::class, 'index'])->name('home');
+
+/*Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
         'laravelVersion' => Application::VERSION,
         'phpVersion' => PHP_VERSION,
     ]);
-});
+});*/
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
@@ -35,4 +36,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
