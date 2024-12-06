865fe3a88b762736ffd46cd6aedc0a902d81183c
Александр Жигунов <karatishka2002@gmail.com>
2024-12-06 06:24:43 +0300

    add migration

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Support\Str;
 
 class Article extends Model
@@ -23,6 +24,16 @@ class Article extends Model
     public function tags(): HasMany
     {
         return $this->hasMany(Tag::class);
+    }
+
+    public function view(): HasOne
+    {
+        return $this->hasOne(View::class);
+    }
+
+    public function like(): HasOne
+    {
+        return $this->hasOne(Like::class);
     }
 }
 
